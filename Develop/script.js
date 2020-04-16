a6f1b65a5d5584d407a8ad0eecdba91a84797694
Thor40 <nicholas.kosik@gmail.com>
2020-04-16 16:27:57 -0500

    fix div

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,33 @@
+var tasks = {};
+
 var date = function(){
     const now = moment().format('dddd, MMMM Do YYYY')
     $("#currentDay").text(now);
 };
 date()
+
+var checkSlot = function(taskText, taskDate, taskList) {
+    var taskLi = $("<span>").addClass("d-flex flex-column badge badge-primary badge-pill");
+
+    var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
+  
+    var taskP = $("<p>").addClass("m-1").text(taskText);
+    
+    taskLi.append(taskSpan, taskP);
+
+    timeHandler(taskLi);
+
+      // append to ul list on the page
+  $("#list-" + taskList).append(taskLi);
+};
+
+
+var timeHandler = function(taskEl) {
+    var hour = $(taskEl).find("span").text().trim();
+    console.log(hour);
+}
+// var hour = $("span").text();
+// console.log(hour)
+
+// var time = moment().format('HH:mm');
+// console.log(time)
