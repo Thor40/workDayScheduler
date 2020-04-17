e4181f4b7d67f41ddbc3c5a96f623519d37f444a
Thor40 <nicholas.kosik@gmail.com>
2020-04-17 12:54:12 -0500

    input/button/localStorage

--- Develop/script.js
+++ Develop/script.js
@@ -6,22 +6,39 @@ var date = function(){
 };
 date()
 
-var checkSlot = function(taskText, taskDate, taskList) {
-    var taskLi = $("<span>").addClass("d-flex flex-column badge badge-primary badge-pill");
+// $("task-text").on("click", function(event) {
+//     var toDoTask = $("#task-text")
+//         .val()
+//         .trim();
 
-    var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
-  
-    var taskP = $("<p>").addClass("m-1").text(taskText);
-    
-    taskLi.append(taskSpan, taskP);
+//     tasks.push(toDoTask);
 
-    timeHandler(taskLi);
+//     localStorage.setItem(toDoTask, JSON.stringify(value));
+// })
 
-      // append to ul list on the page
-  $("#list-" + taskList).append(taskLi);
+// var tasks = JSON.parse(localStorage.getItem("tasklist")) || [];
+// var tasksEl = JSON.parse(localStorage.getItem(id, value))
+
+$('button').on("click", function() {
+    $('input').each(function() {
+        var id = $(this).attr('id');
+        var value = $(this).val();
+
+        localStorage.setItem(id, JSON.stringify(value));
+    })
+
+});
+
+var load = function() {
+    $('input[type="text"').each(function(){
+        var id = $(this).attr('id');
+        var value = JSON.parse(localStorage.getItem(id))
+        
+        $(this).val(value);
+    })
 };
 
-
+load();
 var timeHandler = function(taskEl) {
     var hour = $(taskEl).find("span").text().trim();
     console.log(hour);
@@ -31,3 +48,16 @@ var timeHandler = function(taskEl) {
 
 // var time = moment().format('HH:mm');
 // console.log(time)
+
+
+//     var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
+  
+//     var taskP = $("<p>").addClass("m-1").text(taskText);
+    
+//     taskLi.append(taskSpan, taskP);
+
+//     timeHandler(taskLi);
+
+//       // append to ul list on the page
+//   $("#list-" + taskList).append(taskLi);
+// };
