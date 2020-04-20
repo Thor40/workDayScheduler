628548448b6b324631af40cd8ff9838559c3cc6e
Thor40 <nicholas.kosik@gmail.com>
2020-04-19 19:10:43 -0500

    final commit

--- Develop/script.js
+++ Develop/script.js
@@ -27,58 +27,23 @@ var load = function() {
     })
 };
 
+$("span.hour-item").each(function() {
+    var id = $(this).attr('id');
+    var timeCheck = moment(JSON.stringify(id), "HH:mm a");
+    timeBetween = currentTime.isAfter(timeCheck)
+        if(timeBetween) {
+            $("div[id=task-text-div]").each(function() {
+            $(this).addClass(' bg-secondary');
+        })
+        } else if(!timeBetween) {
+            $("div[id=task-text-div]").each(function() {
+            $(this).addClass(' bg-danger');
+        })
+        } else {
+            $("div[id=task-text-div]").each(function() {
+                $(this).addClass(' bg-primary');
+            })
+        };
+});
+
 load();
-
-    $("span.hour-item").each(function() {
-        var id = $(this).attr('id');
-        var timeCheck = moment(JSON.stringify(id), "HH:mm a");
-        timeBetween = currentTime.isAfter(timeCheck)
-            if(timeBetween) {
-                $("div[id=task-text-div]").each(function() {
-                $(this).addClass(' bg-secondary');
-            })
-            } else if(!timeBetween) {
-                $("div[id=task-text-div]").each(function() {
-                $(this).addClass(' bg-danger');
-            })
-            } else {
-                $("div[id=task-text-div]").each(function() {
-                    $(this).addClass(' bg-primary');
-                })
-            };
-    });
-// $('span[id^="hour_"]', '#hour-item').each(function(index) {
-//     var hourEl = (this.id);
-//     $('#' + hourEl).html(i + '. ');
-
-//     i++;
-//     console.log(hourEl);
-//     });
-
-
-
-// var timeHandler = function() {
-//     var hour = $(taskEl).find("span").text().trim();
-//     console.log(hour);
-// }
-
-
-
-// var hour = $("span").text();
-// console.log(hour)
-
-// var time = moment().format('HH:mm');
-// console.log(time)
-
-
-//     var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
-  
-//     var taskP = $("<p>").addClass("m-1").text(taskText);
-    
-//     taskLi.append(taskSpan, taskP);
-
-//     timeHandler(taskLi);
-
-//       // append to ul list on the page
-//   $("#list-" + taskList).append(taskLi);
-// };
